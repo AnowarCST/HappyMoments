4c0f8ac621603ea7a018df859cfee8a98d51c6fa
Anowar Hossain <anowar@batworld.com>
2015-04-10 16:32:19 +0600

    import

---
+++ src/com/batworld/happymoments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.batworld.happymoments;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap {
+
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+
+    }
+
+}
